c0346db0e18af0d6c5e0fac35e8f519c8508b7d2
David SPORN <sporniket.studio@gmail.com>
2020-11-05 22:38:48 +0100

    generation of symbols library

--- testOnBlitter.bash
+++ testOnBlitter.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-./tools/jsonFromDatasheet.py atari-16-32-bits/src-datasheets/st_blitter.md st_blitter.json && ./tools/libItemFromJsonDatasheet.py st_blitter.json st_blitter.symbol && cat st_blitter.symbol
+./tools/jsonFromDatasheet.py atari-16-32-bits/src-datasheets/st_blitter.md build/st_blitter.json && ./tools/libItemFromJsonDatasheet.py build/st_blitter.json build/st_blitter.symbol && ./tools/compileSymbols.py build/*.symbol build/retro-computer--test.lib && cat build/retro-computer--test.lib
---
+++ tools/compileSymbols.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#
+# Collect a list of symbol description files into a kicad symbol library.
+#
+# Usage : compileSymbols.py input_file1 input_file2 ... output_file
+# * input_fileX : the symbol files to collect.
+# * output_file : the library file to produce.
+
+import sys
+import re
+import json
+
+# check usage
+if len(sys.argv) < 3:
+    print 'Usage : compileSymbols.py input_file output_file'
+    exit()
+    pass
+
+indexOfOutput=len(sys.argv) - 1
+# parse args
+comArgs = {}
+comArgs['inputs']=sys.argv[1:indexOfOutput]
+comArgs['output']=sys.argv[indexOfOutput]
+# Using readlines()
+#srcFile = open(comArgs['input'], 'r')
+#srcLines = srcFile.readlines()
+fmtBegin = 'EESchema-LIBRARY Version 2.4\n#encoding utf-8\n#\n'
+fmtPrefixSymbol = '#\n# {}\n#\n'
+fmtEnd = '#\n#End Library\n'
+
+
+
+with open(comArgs['output'], 'w') as outfile:
+    outfile.write(fmtBegin)
+    for input in comArgs['inputs']:
+        outfile.write(fmtPrefixSymbol.format(input))
+        with open(input, 'r') as srcFile:
+            srcLines = srcFile.readlines()
+            outfile.writelines(srcLines)
+    outfile.write(fmtEnd)
