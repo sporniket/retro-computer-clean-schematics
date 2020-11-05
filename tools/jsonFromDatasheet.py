7ce9ef88552f30583db8ebb56392dfd3eb32be08
David SPORN <sporniket.studio@gmail.com>
2020-11-05 18:57:14 +0100

    added python scripts

---
+++ tools/jsonFromDatasheet.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+#
+# Convert a markdown document following the format of datasheet into a json model of the IC.
+#
+# Usage : symbolsetFromDatasheet.py input_file output_file
+# * input_file : the markdown file to parse.
+# * output_file : the json file to produce.
+
+import sys
+import re
+import json
+
+
+# check usage
+if len(sys.argv) < 3:
+    print 'Usage : symbolsetFromDatasheet.py input_file output_file'
+    exit()
+    pass
+
+# parse args
+comArgs = {}
+comArgs['input']=sys.argv[1]
+comArgs['output']=sys.argv[2]
+# Using readlines()
+srcFile = open(comArgs['input'], 'r')
+srcLines = srcFile.readlines()
+
+# parse source
+reDefineAliases=re.compile('^aliases[ ]*\:.*', re.I)
+reDefineReference=re.compile('^reference[ ]*\:.*', re.I)
+reSectionPinout=re.compile('^pinout',re.I)
+reSectionPinGroup=re.compile('^pin groups',re.I)
+reTableHeadMark=re.compile('^\|[-]{3,}\|.*')
+
+def stripAll(stringList):
+    return list(map(lambda s: s.strip(),stringList))
+
+# Format a single signal name
+def formatSignalName(name):
+    if name.startswith('/'):
+        return '~'+name[1:]+'~'
+    return name
+
+# Format a list of signal names (space separated values)
+# * tokenize
+# * items prefixed by '/' are delimited by '~' instead
+def formatSignalNames(ssvNames):
+    names=ssvNames.split(' ')
+    return "/".join(map(lambda n: formatSignalName(n), filter(lambda n: len(n)>0, names)))
+
+# ======= state-driven behaviour =======
+# 0 = header parts
+# 1 = pinout section, wait pinout data
+# 2 = pinout data
+# 3 = end of pinout data, wait for pin groups section
+# 4 = pin groups section, wait pin groups data
+# 5 = pin groups data
+# 6 = end of pin groups data
+state=0
+
+# ======= behaviour according to parsing state =======
+def handleHeaderLine(line,state):
+    if line.startswith('# '):
+        symMeta['name'] = line[2:].strip().replace(' ','_')
+        return state
+    if line.startswith('> '):
+        return state
+    if line.startswith('* '):
+        line = line[2:].strip()
+        if reDefineAliases.match(line):
+            symMeta['aliases']=stripAll(line.split(':',1)[1].split(','))
+            return state
+        if reDefineReference.match(line):
+            symMeta['reference']=line.split(':',1)[1].strip()
+            return state
+    if line.startswith('## '):
+        line = line[3:].strip()
+        if reSectionPinout.match(line):
+            return  state+1
+    return state
+
+def handlePinoutSectionLine(line,state):
+    if reTableHeadMark.match(line):
+        return state+1
+    return state
+
+def regGroupIfNewAndReturnIt(groupName):
+    if len(groupName) == 0:
+        return None
+    if not groupName in symGroups:
+        symGroups[groupName]={
+            'name':groupName,
+            'rank':-1,
+            'comment':'',
+            'I':0,
+            'O':0,
+            'B':0
+        }
+    return symGroups[groupName]
+
+def handlePinoutData(line,state):
+    if len(line) == 0:
+        return state + 1
+    fields=stripAll(line.split('|'))
+    if len(fields) >= 6:
+        pin={
+            'index':int(fields[1]),
+            'name':formatSignalNames(fields[2]),
+            'type':fields[3].upper(),
+            'group':fields[4].upper(),
+            'comment':fields[5]
+        }
+        symPins.append(pin)
+        effectiveGroup=pin['group']
+        if len(effectiveGroup) > 0:
+            group = regGroupIfNewAndReturnIt(effectiveGroup)
+            pindir = pin['type'][0:1]
+            group[pindir] += 1
+            return state
+        # regroup power/gnd/dnc pins in their own reserved groups
+        effectiveGroup='__'+pin['type']
+        pin['group']=effectiveGroup
+    return state
+
+def handleEndOfPinoutData(line,state):
+    if line.startswith('### '):
+        line = line[4:]
+        if reSectionPinGroup.match(line):
+            return state + 1
+    return state
+
+def handlePinGroupSection(line,state):
+    if reTableHeadMark.match(line):
+        return state+1
+    return state
+
+def handlePinGroupData(line,state):
+    if len(line) == 0:
+        return state + 1
+    fields=stripAll(line.split('|'))
+    if len(fields) >= 4:
+        groupName = fields[1].upper()
+        if not groupName in symGroups:
+            return state
+        group = symGroups[groupName]
+        if len(fields[2]) > 0:
+            group['rank'] = int(fields[2])
+        if len(fields[3]) > 0:
+            group['comment'] = fields[3]
+    return state
+
+def handleEndOfPinGroupData(line,state):
+    return state
+
+#  ======= setup behaviour dispatcher =======
+stateHandler = [
+    handleHeaderLine,
+    handlePinoutSectionLine,
+    handlePinoutData,
+    handleEndOfPinoutData,
+    handlePinGroupSection,
+    handlePinGroupData,
+    handleEndOfPinGroupData
+]
+
+count = 0
+symMeta={}
+symPins=[]
+symGroups={}
+symMetrics={}
+# Strips the newline character
+for line in srcLines:
+    line = line.strip()
+    state = stateHandler[state](line,state)
+    # print("Line {} : {}".format(count, line))
+    count += 1
+
+# build final object
+symbole={
+    'meta':symMeta,
+    'pins':symPins,
+    'pinGroups':symGroups,
+    'metrics':symMetrics
+}
+result = json.dumps(symbole,indent=2)
+with open(comArgs['output'], 'w') as outfile:
+    outfile.write(result)
---
+++ tools/libItemFromJsonDatasheet.py
@@ -0,0 +1,295 @@
+#!/usr/bin/python
+#
+# Convert a markdown document following the format of datasheet into a json model of the IC.
+#
+# Usage : symbolsetFromDatasheet.py input_file output_file
+# * input_file : the markdown file to parse.
+# * output_file : the json file to produce.
+
+
+import sys
+import re
+import json
+
+
+# check usage
+if len(sys.argv) < 3:
+    print 'Usage : symbolsetFromDatasheet.py input_file output_file'
+    exit()
+    pass
+
+# parse args
+comArgs = {}
+comArgs['input']=sys.argv[1]
+comArgs['output']=sys.argv[2]
+
+# Using readlines()
+srcFile = open(comArgs['input'], 'r')
+srcDatasheet = json.load(srcFile)
+
+# Each pin is stored as a "slot item"
+# A slot contains slot items, either pins or empty space
+slotInputs=[]
+slotOutputs=[]
+slotMixed=[]
+slotBusLeft=[]
+slotBusRight=[]
+slotPower=[]
+slotGround=[]
+# final layout
+slotOnLeft=[]
+slotOnRight=[]
+#
+
+def qualifyGroup(g):
+    typeKey = 0;
+    if g['I']>0:
+        typeKey += 1
+    if g['O']>0:
+        typeKey +=10
+    if g['B']>0:
+        typeKey += 100
+    g['qualifier']=typeKey
+
+def computeGroupSortKey(g):
+    return '{:03d}'.format(g['qualifier'])+'_'+'{:04d}'.format(g['rank'])+'_'+g['name']
+
+# first, do the sorting of groups for effective rank
+srcGroups = srcDatasheet['pinGroups'].values()
+for g in srcGroups:
+    qualifyGroup(g)
+
+groupsRanked = sorted(filter(lambda g:g['rank']>=0, srcGroups), key=lambda g:computeGroupSortKey(g))
+groupsUnranked = sorted(filter(lambda g:g['rank']<0, srcGroups), key=lambda g:computeGroupSortKey(g))
+
+
+# Regroup groups by types
+def collectGroupsBySameQualifier(qualifier, rankeds, unrankeds):
+    result = []
+    result.extend(filter(lambda g:g['qualifier']==qualifier,rankeds))
+    result.extend(filter(lambda g:g['qualifier']==qualifier,unrankeds))
+    # reorder elements
+    rerank=1
+    for g in result:
+        g['rank']=rerank
+        rerank += 1
+    return result
+
+def dumpJson(obj,title):
+    print '========<[ {} ]>========'.format(title)
+    print(json.dumps(obj,indent=2))
+    print '================================'
+
+groupInputs = collectGroupsBySameQualifier(1, groupsRanked, groupsUnranked)
+groupOutputs = collectGroupsBySameQualifier(10, groupsRanked, groupsUnranked)
+groupMixeds = collectGroupsBySameQualifier(11, groupsRanked, groupsUnranked)
+groupBidis = collectGroupsBySameQualifier(100, groupsRanked, groupsUnranked)
+
+#
+# metrics settings (to be loaded in the future) in mils (1000 mil = 1 inch)
+metrics = {
+    'font':{
+        'line-height':100,
+        'glyphWidthLastDecile':75 # 90% of the glyphes are less broad or equal to 75 mil
+    },
+    'common':{
+        'margin':100,
+        'padding':0
+    },
+    'power':{
+        # power pins should be connected to decoupling capacitor, make some clearance for them.
+        'margin':200,
+        'padding':400
+    }
+}
+
+# fill sections of pins
+isInputPin=lambda p:'I' == p['type'][0:1]
+isOutputPin=lambda p:'O' == p['type'][0:1]
+
+def min(a,b):
+    return a if a < b else b
+
+def max(a,b):
+    return a if a > b else b
+
+def newSectionEnd():
+    return {
+        'items':[],
+        'width':0
+    }
+
+def newSection():
+    return {
+        'left':newSectionEnd(),
+        'right':newSectionEnd(),
+        'height':0
+    }
+
+def newSectionPower():
+    return {
+        'power':newSectionEnd(),
+        'ground':newSectionEnd(),
+        'width':0
+    }
+
+reBusWireIndexInName=re.compile('[0-9]+$')
+
+def collectPinsOfGroup(pins, groupName):
+    result = filter(lambda p:p['group'] == groupName,pins)
+    # If the group of pins looks like a bus, the pin are sorted by descending index extracted from pin names
+    isBusGroup = True
+    for p in result:
+        busWireIndexMatch = reBusWireIndexInName.search(p['name'])
+        p['busIndex'] = None if None == busWireIndexMatch else int(busWireIndexMatch.group(0))
+        p['isBusWire'] = False if None == busWireIndexMatch else True
+        isBusGroup = isBusGroup and p['isBusWire']
+    if isBusGroup:
+        result.sort(key=lambda p:p['busIndex'], reverse=True)
+    # done
+    return result
+
+def collectPinsOfGroups(pins, groups):
+    return reduce(lambda a, b : a + [None] + b, map(lambda g:collectPinsOfGroup(pins, g['name']), groups))
+
+def widthOfSectionEnd(items):
+    return reduce(max, map(lambda i: len(i['name']), filter(lambda i: not i == None, items)))
+
+
+
+def setSectionEndItems(sectionEnd, items):
+    if len(items) > 0:
+        sectionEnd['items'].extend(list(items))
+        sectionEnd['width'] = widthOfSectionEnd(sectionEnd['items'])
+
+def updateSectionHeight(section,end1,end2):
+    section['height']=max(len(section[end1]['items']), len(section[end2]['items']))
+
+pins = srcDatasheet['pins']
+totalLineHeight=0
+totalMinimalWidth=0
+
+def createSectionFromCollectionOfGroups(pins,groupsOnEnd1,groupsOnEnd2,endName1,endName2):
+    section = newSection()
+    if len(groupsOnEnd1) > 0:
+        setSectionEndItems(section[endName1],collectPinsOfGroups(pins, groupsOnEnd1))
+    if len(groupsOnEnd2) >0:
+        setSectionEndItems(section[endName2],collectPinsOfGroups(pins, groupsOnEnd2))
+    updateSectionHeight(section,endName1,endName2)
+    return section
+
+# section of pure input groups and pure output groups
+sectionPureUnidirectionGroups = createSectionFromCollectionOfGroups(pins,groupInputs,groupOutputs,'left','right')
+
+totalLineHeight += len(sectionPureUnidirectionGroups['left']['items']) + 1
+totalLineHeight += len(sectionPureUnidirectionGroups['right']['items'])
+
+totalMinimalWidth = max(totalMinimalWidth, sectionPureUnidirectionGroups['left']['width'] + sectionPureUnidirectionGroups['right']['width'])
+
+# for each groups of mixed input/output pins, create a section
+def dispatchIoPinsIntoSectionEnds(pins,section,endInput,endOutput):
+    inputs=filter(isInputPin, pins)
+    outputs=filter(isOutputPin, pins)
+    if len(inputs) > 0:
+        setSectionEndItems(section[endInput],inputs)
+    if len(outputs) > 0:
+        setSectionEndItems(section[endOutput],outputs)
+    updateSectionHeight(section,endInput,endOutput)
+
+def createSectionFromMixedIoPinsGroup(pins,group,endName1,endName2):
+    result = newSection()
+    pinsubset=collectPinsOfGroup(pins,group['name'])
+    dispatchIoPinsIntoSectionEnds(pinsubset,result,endName1,endName2)
+    return result
+
+
+sectionsOfMixedPinsGroups = map(lambda g:createSectionFromMixedIoPinsGroup(pins,g,'left','right'), groupMixeds)
+totalLineHeight += reduce(lambda a,b:a + b + 1, map(lambda s: s['height'],sectionsOfMixedPinsGroups))
+totalMinimalWidth = reduce(lambda a,b:max(a,b), map(lambda s:s['left']['width'] + s['right']['width'], sectionsOfMixedPinsGroups))
+
+
+# create a section with bidi groups dispatched between the left and the right side. The first group is on the left,
+# the second one is on the right, and so on...
+keepEvenGroups = lambda g:0 == (g['rank'] % 2)
+keepOddGroups = lambda g:not keepEvenGroups(g)
+
+groupsBidiOnLeft = filter(keepOddGroups,groupBidis)
+groupsBidiOnRight = filter(keepEvenGroups,groupBidis)
+
+sectionBidis = createSectionFromCollectionOfGroups(pins,groupsBidiOnLeft,groupsBidiOnRight,'left','right')
+totalLineHeight += sectionBidis['height']
+totalMinimalWidth = max(totalMinimalWidth,sectionBidis['left']['width'] + sectionBidis['right']['width'])
+
+# Generate symbol description according to kicad format
+fmtBeginSymbol='DEF {} U 0 50 Y Y 1 L N\n' # name
+fmtAlias='ALIAS {}\n' # names (ssv)
+fmtField='F{} "{}" {} {} 50 H V C CNN\n' # field index, name, x, y
+fmtBeginDraw='DRAW\n'
+fmtSurface='S -{0} {1} {0} -{1} 0 1 10 f\n' # half width, half height
+fmtPinWest='X {} {} -{} {} 300 R 50 50 0 0 {}\n' # name, index, half width + 300, y, pin type
+fmtPinEast='X {} {} {} {} 300 L 50 50 0 0 {}\n' # name, index, half width + 300, y, pin type
+fmtPinNorth='X {} {} {} {} 300 D 50 50 0 0 {}\n' # name, index, x, half height + 300, pin type
+fmtPinSouth='X {} {} {} -{} 300 U 50 50 0 0 {}\n' # name, index, x, half height + 300, y, pin type
+fmtEndDraw='ENDDRAW\n'
+fmtEndSymbol='ENDDEF\n'
+
+# pin type to kicad type
+pinTypeToElecType={
+    'PWR' : 'W',
+    'GND' : 'w',
+    'DNC' : 'N',
+    'I' : 'I',
+    'ICLK' : 'I',
+    'O' : 'O',
+    'OCLK' : 'O',
+    'O3' : 'T',
+    'OCOL' : 'C',
+    'OEMT' : 'E',
+    'OPWR' : 'w',
+    'B' : 'B'
+}
+
+# output symbol
+halfWidth=metrics['font']['glyphWidthLastDecile'] * totalMinimalWidth / 2 + metrics['common']['margin']
+halfWidth += 50 - (halfWidth % 50) # snap to grid
+pinStartH=halfWidth + 300
+halfHeight=metrics['font']['line-height'] * totalLineHeight / 2 + metrics['common']['margin']
+halfHeight += 50 - (halfHeight % 50)
+ySection = halfHeight - metrics['common']['margin'] / 2
+
+def outputPinsOfSectionEndHoriz(pins, fmt, yStart,outfile):
+    yLine = yStart
+    for pin in pins:
+        if not pin == None:
+            outfile.write(fmt.format(pin['name'], pin['index'], pinStartH, yLine, pinTypeToElecType[pin['type']]))
+        yLine -= metrics['font']['line-height']
+
+with open(comArgs['output'], 'w') as outfile:
+    outfile.write(fmtBeginSymbol.format(srcDatasheet['meta']['name'].upper()))
+    outfile.write(fmtAlias.format(reduce(lambda a,b:a + ' ' + b, srcDatasheet['meta']['aliases'])))
+    outfile.write(fmtField.format(0,srcDatasheet['meta']['reference'], -halfWidth + 300, halfHeight + 400))
+    outfile.write(fmtField.format(1,srcDatasheet['meta']['name'], -halfWidth + 300, halfHeight + 300))
+    outfile.write(fmtBeginDraw)
+    outfile.write(fmtSurface.format(halfWidth,halfHeight))
+
+    # draw input pins only groups
+    outputPinsOfSectionEndHoriz(sectionPureUnidirectionGroups['left']['items'],fmtPinWest,ySection,outfile)
+    ySection -= (len(sectionPureUnidirectionGroups['left']['items'])+1) * metrics['font']['line-height']
+
+    # draw io pins groups
+    for s in sectionsOfMixedPinsGroups:
+        outputPinsOfSectionEndHoriz(s['left']['items'],fmtPinWest,ySection,outfile)
+        outputPinsOfSectionEndHoriz(s['right']['items'],fmtPinEast,ySection,outfile)
+        ySection -= (s['height'] + 1) * metrics['font']['line-height']
+
+    # draw output pins only groups
+    outputPinsOfSectionEndHoriz(sectionPureUnidirectionGroups['right']['items'],fmtPinEast,ySection,outfile)
+    ySection -= (len(sectionPureUnidirectionGroups['right']['items'])+1) * metrics['font']['line-height']
+
+    # draw bidis bins groups
+    outputPinsOfSectionEndHoriz(sectionBidis['left']['items'],fmtPinWest,ySection,outfile)
+    outputPinsOfSectionEndHoriz(sectionBidis['right']['items'],fmtPinEast,ySection,outfile)
+    ySection -= (sectionBidis['height'] + 1) * metrics['font']['line-height']
+
+    outfile.write(fmtEndDraw)
+    outfile.write(fmtEndSymbol)
