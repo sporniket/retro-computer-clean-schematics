a46a9c07414c3a255359f221e61d3bfcaba4c87a
David SPORN <sporniket.studio@gmail.com>
2021-06-25 06:11:34 +0200

    added copyright/gpl notice in each scripts

--- buildChipLibrary.bash
+++ buildChipLibrary.bash
@@ -1,5 +1,20 @@
 #!/bin/bash
 #
+# Copyright 2021 David SPORN
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 # generate a symbol library from a single datasheet.
 # $1 : path to the datasheet
 SRC="$1"
